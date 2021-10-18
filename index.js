c1995811dc1d152550ebb29399366c53ebca809c
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-10-17 22:02:33 -0500

    we create the server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('desde index');
+const express = require( 'express' );
+
+// * Crear el Servidor
+const app = express();
+
+// * Puerto de la App
+const PORT = process.env.PORT || 4000;
+
+// TODO: Arranca la app
+app.listen( PORT, () => {
+    console.log( `El servidor esta funcionado en el puerto ${PORT}` );
+});
