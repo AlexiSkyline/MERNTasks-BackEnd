704603d66f03234e8124d9cd5e51ad112b366b8a
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-10-18 23:31:01 -0500

    we add the data insertion in the DB

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,6 +1,22 @@
+const Usuarios = require("../models/Usuarios");
 
 
+exports.crearUsuario = async ( req, res ) => {
+    
+    try {
+        let usuario;
 
-exports.crearUsuario = ( req, res ) => {
-    console.log( req.body );
+        // * Creamos el nuevo usuari
+        usuario = new Usuarios( res.body );
+
+        // * Guardamos usuario
+        await usuario.save();
+
+        // * Mensje de confirmación
+        res.send( 'Usuario creado correctamente' );
+
+    } catch (error) {
+        console.log( error );
+        res.status( 400 ).send( 'Hubo un error' );
+    }
 }
