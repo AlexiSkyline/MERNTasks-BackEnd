3fed559a8f980c4191bde56473e32be5d4e03b55
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-10-19 23:11:33 -0500

    we add the insertion of new projects

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -1,5 +1,15 @@
-
+const Proyecto = require( '../models/Proyecto' );
 
 exports.crearProyecto = async ( req, res ) => {
-    
+
+    try {
+        // Todo: Crea un nuevo proyecto
+        const proyecto = new Proyecto( req.body );
+        proyecto.save();
+        res.json( proyecto );
+        
+    } catch ( error ) {
+        console.log( error );
+        res.status(500).send( 'Hubo un error' );
+    }
 }
